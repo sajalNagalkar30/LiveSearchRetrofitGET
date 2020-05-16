4a8d8f9e794e7a6fcd97152dfabe8103f41b1df7
sajalNagalkar30 <sajal4vinunagalkar@gmail.com>
2020-05-16 22:16:08 +0530

    Initial

--- app/src/main/java/com/haerul/livesearchretrofitget/ApiClient.java
+++ app/src/main/java/com/haerul/livesearchretrofitget/ApiClient.java
@@ -9,7 +9,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
 
-    public static final String BASE_URL = "http://192.168.1.102/retrofit/GET/";
+    public static final String BASE_URL = "https://sajalnagalkar.000webhostapp.com/";
     public static Retrofit retrofit;
 
     public static Retrofit getApiClient(){
